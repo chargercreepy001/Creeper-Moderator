643f5c87df39280a7f4602ea382c901ebd435d97
chargercreepy001 <109840908+chargercreepy001@users.noreply.github.com>
2022-07-29 23:40:34 +0530

    Update maincode.js

--- maincode.js
+++ maincode.js
@@ -9,8 +9,8 @@ console.log("Bot started.");
 
  //describe the Dicord API
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: "GUILDS", "GUILD_MESSAGES", "GUILD_PRESENCES"] });
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GatewayIntentBits } = require('discord.js');
+const client = new Discord.Client({  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildBans] });
 const AntiSpam = require("discord-anti-spam");
 
 
